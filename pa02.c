44605b64d379c2849ceffb5fad16edf2d354c7ba
SamarKaranch <samarkaranch@gmail.com>
2024-07-14 17:51:51 -0400

    Added linked list functions

---
+++ pa02.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+void insert(Node** head, int data) {
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    new_node->data = data;
+    new_node->next = NULL;
+
+    if (*head == NULL) {
+        *head = new_node;
+    } else {
+        Node* current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = new_node;
+    }
+}
+
+void delete(Node** head, int data) {
+    Node* current = *head;
+    Node* previous = NULL;
+
+    while (current != NULL && current->data != data) {
+        previous = current;
+        current = current->next;
+    }
+
+    if (current == NULL) {
+        printf("Node with data %d not found.\n", data);
+        return;
+    }
+
+    if (previous == NULL) {
+        *head = current->next;
+    } else {
+        previous->next = current->next;
+    }
+
+    free(current);
+}
+
+int search(Node* head, int data) {
+    Node* current = head;
+    while (current != NULL) {
+        if (current->data == data) {
+            return 1; 
+        }
+        current = current->next;
+    }
+    return 0; 
+}
+
